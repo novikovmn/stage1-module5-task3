60ed2d0dee37413ad4143e8132bae0c36d887070
novikovmn <genrigale@gmail.com>
2023-03-10 19:00:14 +0300

    2nd attempt

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -3,15 +3,16 @@ package com.epam.mjc;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        return list.stream().filter(num -> num % 2 == 0).toList();
+        return list.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        return list.stream().map(String::toUpperCase).toList();
+        return list.stream().map(String::toUpperCase).collect(Collectors.toList());
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
